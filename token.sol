f79ac7f891f74dbcf3372e5f130be800e63c63a9
joshsoliman <144326238+joshsoliman@users.noreply.github.com>
2024-10-16 21:31:09 +0800

    Update token.sol

--- token.sol
+++ token.sol
@@ -3,24 +3,28 @@ pragma solidity 0.8.18;
 
 contract MyToken {
 
-    // Public variables to store the token details
-    string public tokenName = "MyToken";
-    string public tokenAbbrv = "MTK";
+    //public variables to store the token details
+    string public tokenName = "META";
+    string public tokenAbbrv = "MTA";
     uint public totalSupply = 0;
 
-    // Mapping to keep track of each address's balance
+    //mapping to keep track of each address's balance
     mapping(address => uint) public balances;
 
-    // Mint function to create tokens
+    //mint function to create tokens
     function mint(address _address, uint _value) public {
-        totalSupply += _value;               // Increase the total supply
-        balances[_address] += _value;        // Increase the balance of the specified address
+        totalSupply += _value;               //increase the total supply
+        balances[_address] += _value;        //increase the balance of the specified address
     }
 
     // Burn function to destroy tokens
     function burn(address _address, uint _value) public {
-        require(balances[_address] >= _value, "Insufficient balance to burn"); // Check if the address has enough tokens
-        totalSupply -= _value;               // Decrease the total supply
-        balances[_address] -= _value;        // Decrease the balance of the specified address
+        if (balances[_address] >= _value) { //checks if the address has enough tokens
+            totalSupply -= _value;               //decrease the total supply
+            balances[_address] -= _value;        //decrease the balance of the specified address
+        }
     }
 }
+
+
+
