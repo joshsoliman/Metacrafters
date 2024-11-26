907ab70bc17c43373ce812e83861a4e512544ea6
joshsoliman <144326238+joshsoliman@users.noreply.github.com>
2024-11-26 21:48:04 +0800

    Update types_of_func.sol

--- types_of_func.sol
+++ types_of_func.sol
@@ -7,11 +7,16 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.8.0/contr
 contract MyToken is ERC20, Ownable {
     constructor() ERC20("Josh", "Mint, Burn and Transfer") {}
 
+   
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
-
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
+    }
+
+    function transfer(address to, uint256 amount) public override returns (bool success) {
+        success = super.transfer(to, amount);
+        return success;
     }
 }
